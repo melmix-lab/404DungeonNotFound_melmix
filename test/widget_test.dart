d5156303393f30835f30b5454e572a42cb40e594
melmix-lab <melissiob@gmail.com>
2026-01-21 21:34:26 +0100

    Fix package name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:_404dungeondotdound_fronted/main.dart';
+import 'package:app_404_dungeon/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
